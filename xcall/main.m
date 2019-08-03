42fbdb8bbcc55f70b303e5482a702e2a48e52dd6
lulinglm <lulinglm@163.com>
2019-08-03 15:16:20 +0800

    feat: init project

---
+++ xcall/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  xcall
+//
+//  Created by lulinglm on 03.04.17.
+//  Copyright © 2019 lulinglm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
